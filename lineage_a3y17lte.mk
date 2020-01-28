8eb5693aaadac14555d7072653ae5a411ba61ef9
Astrako <apamarin@gmail.com>
2020-01-28 16:07:37 +0100

    a3y17lte: misc changes

--- lineage_a3y17lte.mk
+++ lineage_a3y17lte.mk
@@ -16,11 +16,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from a3y17lte device
 $(call inherit-product, device/samsung/a3y17lte/device.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -31,6 +30,8 @@ PRODUCT_NAME := lineage_a3y17lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A320FL
 PRODUCT_MANUFACTURER := samsung
+
+TARGET_VENDOR := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
