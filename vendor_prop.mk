029369b831e8279f5a972698c697f0afbac3b38c
Astrako <apamarin@gmail.com>
2020-02-20 16:56:55 +0100

    a3y17lte: misc changes

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -7,10 +7,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320
 
-# DUAL SIM
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.multisim.simslotcount=2
-
 # Fix for echo in calls
 PRODUCT_PROPERTY_OVERRIDES += \
     audio_hal.disable_two_mic=false
