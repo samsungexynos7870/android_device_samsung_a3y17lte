dda4af82b715336cf4b0cd9ef179db035b958c96
Alejandro <apamarin@gmail.com>
2020-06-30 20:14:36 +0200

    a3y17lte: remove PRODUCT_MODEL
    We set right model in libinit

--- aosp_a3y17lte.mk
+++ aosp_a3y17lte.mk
@@ -29,7 +29,6 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 PRODUCT_DEVICE := a3y17lte
 PRODUCT_NAME := aosp_a3y17lte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-G981B
 PRODUCT_MANUFACTURER := samsung
 
 TARGET_VENDOR := samsung
