6a6a4867fbc2b56efea8932750ec3cf0bf488c7e
Alejandro <apamarin@gmail.com>
2020-08-18 09:51:03 +0200

    a3y17lte: split mixer_paths in mixer_paths and mixer_gains

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ PRODUCT_COPY_FILES += \
 
 # Custom mixer_paths
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(DEVICE_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 1280
