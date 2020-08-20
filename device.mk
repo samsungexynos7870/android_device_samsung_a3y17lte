0ac67db8e5a3b6714e2c1434034b70c6a0628f95
Alejandro <apamarin@gmail.com>
2020-08-20 09:18:30 +0200

    a3y17lte: don't build amplifier hal

--- device.mk
+++ device.mk
@@ -36,10 +36,6 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
 
-# Audio
-PRODUCT_PACKAGES += \
-    audio_amplifier.universal7870_32
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
