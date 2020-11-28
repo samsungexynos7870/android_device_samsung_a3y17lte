4e0c9f90b602744e6a5b68afe6601f7a5661691b
TALU <taluemail272@gmail.com>
2020-11-28 15:56:10 +0000

    gtaxl: add public.libraries.txt.
    Moved from vendor.

--- device.mk
+++ device.mk
@@ -272,6 +272,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FlipFlap
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # ADB and USB configuration
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.usb.config=none \
