94d9dd45d3888f74a33197f698c51b729ae15afd
followmsi <followmsi@gmail.com>
2020-12-12 10:54:07 +0100

    gtaxl: Do not update the recovery image
    Change-Id: Ia89d90c729e5265f45ef916ecb9113b00db58ca5

--- system_prop.mk
+++ system_prop.mk
@@ -63,3 +63,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # FIFO UI scheduling
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.use_fifo_ui=1
+
+# Do not update the recovery image
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.recovery_update=false
