a280086ae3341d987e23af1313f721397303f833
Flominator <github.flominatorgd@gmail.com>
2023-07-06 09:51:18 +0200

    a3y17lte: add fix for hotspot on android 11 for qcom wifi by setting WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,7 @@ WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 WPA_SUPPLICANT_USE_HIDL := true
 WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
+WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true
 
 # inherit from common
 -include device/samsung/universal7870-common/BoardConfigCommon.mk
