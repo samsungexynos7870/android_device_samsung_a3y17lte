6f5f6cc45c6fd5af662a13280489f1e4fc815dea
Flominator <github.flominatorgd@gmail.com>
2023-07-06 10:00:22 +0200

    a3y17lte: copy nfc permission android.hardware.nfc.uicc.xml

--- device.mk
+++ device.mk
@@ -20,7 +20,8 @@ DEVICE_PATH := device/samsung/a3y17lte
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml
 
 # Custom mixer_paths
 PRODUCT_COPY_FILES += \
