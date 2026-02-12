c65b861e44c4e9d27d6f7c75be72d9a17f109bfc
Flominator <github.flominatorgd@gmail.com>
2026-02-13 00:27:51 +0100

    a3y17lte: drop deprecated guards

--- device.mk
+++ device.mk
@@ -28,10 +28,6 @@ else ifeq ($(TARGET_DEVICE_HAS_OSS_AUDIO_HAL),true)
 TARGET_DEVICE_HAS_TFA_AMP := true
 endif
 
-# gatekeeper type guard
-TARGET_DEVICE_HAS_HW_GATEKEEPER_BIOMETRICS := true
-TARGET_DEVICE_HAS_HW_GATEKEEPER_COMMON := false
-
 # radio type guard
 TARGET_DEVICE_HAS_SEC_RIL := true
 
@@ -40,13 +36,6 @@ TARGET_DEVICE_HAS_SEC_GNSS := true
 
 # prebuilt slsi
 TARGET_DEVICE_HAS_SAMSUNG_SLSI_EXYNOS7870 := false
-
-# keymaster type guard 
-# TODO: Fix keymaster driver telling its untrusted
-TARGET_DEVICE_HAS_SEC_KEYMASTER := false
-
-# gatekeeper type guard
-TARGET_DEVICE_HAS_HW_GATEKEEPER := true
 
 # Permissions
 PRODUCT_COPY_FILES += \
