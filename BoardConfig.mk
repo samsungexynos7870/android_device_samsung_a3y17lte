f758c7bb5ed11d5bcdf46811d23da5d0bb9464d2
FlominatorGD <github.flominatorgd@gmail.com>
2026-09-24 14:58:53 +0200

    a3y17lte: disable A/B OTA updater

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,6 +95,9 @@ BOARD_CAMERA2_API_SUPPORT := true
 BOARD_CAMERA_SAMSUNG_TN_FEATURE := true
 BOARD_CAMERA_HAL3_FEATURE := true
 
+# A/B
+AB_OTA_UPDATER := false
+
 ## USB
 $(call soong_config_set,samsungUsbGadgetVars,gadget_name,13600000.dwc3)
 
